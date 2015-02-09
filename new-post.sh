c80b7c30e450b86ff9614cd5a5fc730694c9f8f8
Andrew Noyes <anoyes34@gmail.com>
2015-02-09 01:51:24 -0500

    Add new post script.

---
+++ new-post.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+usage() {
+    echo "usage: $0 <title>"
+}
+
+if [ $# -eq 0 ] ; then
+    usage
+    exit 1
+fi
+
+TITLE="$@"
+DATE=$(date +"%Y-%m-%d")
+
+OUTPUTDIR="content/posts"
+
+OUTFILE="$OUTPUTDIR/$(echo $TITLE | sed 's/ /-/g').md"
+
+cat > "$OUTFILE" << EOF
+---
+title:  $TITLE
+date:   $DATE
+categories: []
+---
+
+EOF
+
+exec $EDITOR "$OUTFILE"
